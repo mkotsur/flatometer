be2900aee7ea7a60ee6afdf55348bebaee0d78e1
Mykhailo Kotsur <miccots@gmail.com>
2012-06-23 14:48:06 +0200

    So far so good. List of all flats is a nice ng-repeat example.

---
+++ js/flatometer.js
@@ -0,0 +1,15 @@
+var FlatsCtl = function($scope) {
+
+    $scope.flats = [
+        {
+            address: "Eerste Weteringplantsoen 8, Amsterdam",
+            note: "That's where WLL take place"
+        },
+        {
+            address: "Herengracht 83, Amsterdam",
+            note: "Probably very fancy building"
+        }
+    ];
+
+    return this;
+};
